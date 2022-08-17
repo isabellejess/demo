5d0f9eeafc2d93525a0623ed3b5c08105c216548
isabellejess <isabellejt03@gmail.com>
2022-08-18 00:14:38 +0700

    demo ukm expo

--- test1.cpp
+++ test1.cpp
@@ -5,25 +5,26 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv) {
-    VideoCapture vid("video/bola.mp4");
+    VideoCapture vid("video/demo.mp4");
     Mat frame,tresh,frame_resize,frame_hsv;
     int L_H = 0;
-    int U_H = 73;
-    int L_S = 0;
+    int U_H = 7;
+    int L_S = 40;
     int U_S = 255;
-    int L_V = 0;
+    int L_V = 70;
     int U_V = 255;
-    namedWindow("atur", WINDOW_AUTOSIZE);
-    createTrackbar ("L_H", "atur", &L_H, 179);
-    createTrackbar ("U_H", "atur", &U_H, 179);
-    createTrackbar ("L_S", "atur", &L_S, 255);
-    createTrackbar ("U_S", "atur", &U_S, 255);
-    createTrackbar ("L_V", "atur", &L_V, 255);
-    createTrackbar ("U_V", "atur", &U_V, 255);
+    // namedWindow("atur", WINDOW_AUTOSIZE);
+    // createTrackbar ("L_H", "atur", &L_H, 179);
+    // createTrackbar ("U_H", "atur", &U_H, 179);
+    // createTrackbar ("L_S", "atur", &L_S, 255);
+    // createTrackbar ("U_S", "atur", &U_S, 255);
+    // createTrackbar ("L_V", "atur", &L_V, 255);
+    // createTrackbar ("U_V", "atur", &U_V, 255);
 
     while (true){
         vid.read(frame);
-        resize (frame,frame_resize,Size(),0.5,0.5);
+        resize(frame,frame_resize,Size(),0.5,0.5);
+        rotate(frame_resize,frame_resize,ROTATE_90_CLOCKWISE);
         cvtColor(frame_resize,frame_hsv, COLOR_BGR2HSV);        
         inRange(frame_hsv, Scalar (L_H, L_S, L_V), Scalar(U_H, U_S, U_V), tresh);
 
@@ -36,15 +37,15 @@ int main(int argc, char** argv) {
             float radius;
             contours_lagi = contours[i];
             minEnclosingCircle (contours_lagi,center,radius);
-            if (radius>95 && radius <189){
+            if (radius>40 && radius <100){
                 circle(frame_resize,center,radius,Scalar(0,225,0),5);
-                //printf ("x= %.2f, y= %.2f, rad= %.2f\n\n", center.x, center.y, radius);
+                printf ("x= %.2f, y= %.2f, rad= %.2f\n\n", center.x, center.y, radius);
             }
         }
         imshow("HASIL",frame_resize);
         
 
-        if ((char)27 == (char) waitKey(10))
+        if ((char)27 == (char) waitKey(20))
         break;
     }
     return 0;
